624f0f48a95feb3cf424a645d60b10954c3f179f
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2015-09-19 15:50:23 +0530

    Cleaned up docs, prepare for release

--- release.cmd
+++ release.cmd
@@ -1,11 +1,4 @@
 rmdir/s/q build
 cmd /c "envset x86 && cd src && tclsh build.tcl extension -target win32-ix86-cl && cd .."
 cmd /c "envset x64 && cd src && tclsh build.tcl extension -target win32-x86_64-cl && cd .."
-
- 
- 
- 
-
- 
- 
- 
+cmd /c "cd src && tclsh build.tcl tea && cd .."
