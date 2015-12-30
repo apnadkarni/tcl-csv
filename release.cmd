02b2882b9abd3f89a90e5ae63e017e68af730933
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2015-12-30 21:34:28 +0530

    Automate appending version to release files

--- release.cmd
+++ release.cmd
@@ -1,8 +1,11 @@
 rmdir/s/q build
+@rem Form the file name basaed on the version we are building
+@for /f %%i in ('tclsh src/version.tcl') do set FNAME=tclcsv-%%i
+
 @rem We fire off new shells so as to not change our env
 cmd /c "envset x86 && cd src && tclsh build.tcl extension -target win32-ix86-cl && cd .."
 cmd /c "envset x64 && cd src && tclsh build.tcl extension -target win32-x86_64-cl && cd .."
 cmd /c "cd src && tclsh build.tcl tea && cd .."
-cd build\lib && zip -r tclcsv.zip tclcsv && move tclcsv.zip .. && cd ..\..
-cd build\tea && tar cvf tclcsv.tar tclcsv && gzip tclcsv.tar && move tclcsv.tar.gz .. && cd ..\..
+cd build\lib && zip -r tclcsv.zip tclcsv && move tclcsv.zip ..\%FNAME%.zip && cd ..\..
+cd build\tea && tar cvf tclcsv.tar tclcsv && gzip tclcsv.tar && move tclcsv.tar.gz ..\%FNAME%.tar.gz && cd ..\..
 
--- src/version.tcl
+++ src/version.tcl
@@ -1,4 +1,7 @@
 namespace eval tclcsv {
     proc version {} {return 2.2.0}
 }
+if {[file tail [info script]] eq [file tail [lindex $argv0 0]]} {
+    puts [tclcsv::version]
+}
 return [tclcsv::version]
