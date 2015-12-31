8721ea1bb999b2519668a4f63d062b608a1339ea
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2015-12-31 08:17:46 +0530

    Fix version.tcl to check whether in safe interp before outputting version string. Fix example script in docs

--- src/version.tcl
+++ src/version.tcl
@@ -1,8 +1,11 @@
 namespace eval tclcsv {
     proc version {} {return 2.2.0}
-}
-# Print version if this file is the main script. Used during builds.
-if {[file tail [info script]] eq [file tail [lindex $argv0 0]]} {
-    puts [tclcsv::version]
+    # Print version if this file is the main script. Used during builds.
+    # If sourced inside a safe interp argv0 will not exist so check for that
+    # as well.
+    if {[info exists ::argv0] &&
+        [file tail [info script]] eq [file tail [lindex $::argv0 0]]} {
+        puts [version]
+    }
 }
 return [tclcsv::version]
