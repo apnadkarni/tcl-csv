dcd11cfa5e695093936dc8080159ff682643069c
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2016-01-03 17:10:45 +0530

    Tweak distro directory name

--- release.cmd
+++ release.cmd
@@ -6,6 +6,8 @@ rmdir/s/q build
 cmd /c "envset x86 && cd src && tclsh build.tcl extension -target win32-ix86-cl && cd .."
 cmd /c "envset x64 && cd src && tclsh build.tcl extension -target win32-x86_64-cl && cd .."
 cmd /c "cd src && tclsh build.tcl tea && cd .."
-cd build\lib && zip -r tclcsv.zip tclcsv && move tclcsv.zip ..\%FNAME%.zip && cd ..\..
-cd build\tea && tar cvf tclcsv.tar tclcsv && gzip tclcsv.tar && move tclcsv.tar.gz ..\%FNAME%.tar.gz && cd ..\..
+move build\lib\tclcsv build\lib\%FNAME%
+cd build\lib && zip -r %FNAME%.zip %FNAME% && move %FNAME%.zip .. && cd ..\..
+move build\tea\tclcsv build\tea\%FNAME%
+cd build\tea && tar cvf ../%FNAME%.tar %FNAME% && gzip ../%FNAME%.tar && cd ..\..
 
