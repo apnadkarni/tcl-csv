bc9ef10cdaaa3a9cec5c967d7c6bbd166cffe793
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2018-03-12 12:14:21 +0530

    Update release.cmd for nmake based build, fix readme, add license

--- doc/README.md
+++ doc/README.md
@@ -1,1 +1,20 @@
-Please see https://tclcsv.magicsplat.com for documentation.
+# Tclcsv extension for Tcl
+
+Tclcsv is a binary extension for reading and writing CSV format files.
+It also includes a Tk widget for configuring CSV formats.
+
+## Documentation
+
+See <https://tclcsv.magicsplat.com> for documentation.
+
+## Downloads
+
+Downloads are available from
+<https://sourceforge.net/projects/tclcsv/files/>.
+
+The file tclcsv-VERSION.zip contains Windows binaries for both 32-bit
+and 64-bit systems.
+
+The files tclcsv-VERSION-src.zip and tclcsv-VERSION-src.tar.gz 
+contain source distributions.
+
--- release.cmd
+++ release.cmd
@@ -1,13 +1,20 @@
 rmdir/s/q build
-@rem Form the file name based on the version we are building
-@for /f %%i in ('tclsh src/version.tcl') do set FNAME=tclcsv%%i
 
 @rem We fire off new shells so as to not change our env
-cmd /c "envset x86 && cd src && tclsh build.tcl extension -target win32-ix86-cl && cd .."
-cmd /c "envset x64 && cd src && tclsh build.tcl extension -target win32-x86_64-cl && cd .."
-cmd /c "cd src && tclsh build.tcl tea && cd .."
-move build\lib\tclcsv build\lib\%FNAME%
-cd build\lib && zip -r %FNAME%.zip %FNAME% && move %FNAME%.zip .. && cd ..\..
-move build\tea\tclcsv build\tea\%FNAME%
-cd build\tea && tar cvf ../%FNAME%.tar %FNAME% && gzip ../%FNAME%.tar && cd ..\..
+cmd /c "envset x86 && cd win && nmake /f makefile.vc INSTALLDIR=..\build TCLDIR=d:\tcl\868-vc6\x86 clean && cd .."
+cmd /c "envset x86 && cd win && nmake /f makefile.vc INSTALLDIR=..\build TCLDIR=d:\tcl\868-vc6\x86 tclcsv install && cd .."
+cmd /c "envset x64 && cd win && nmake /f makefile.vc INSTALLDIR=..\build TCLDIR=d:\tcl\868-vc6\x64 clean && cd .."
+cmd /c "envset x64 && cd win && nmake /f makefile.vc INSTALLDIR=..\build TCLDIR=d:\tcl\868-vc6\x64 tclcsv install && cd .."
 
+@rem Extract the version number and build the file name to use
+for /f %%i in ('win\nmakehlp -V configure.in tclcsv') do set TCLCSVVER=%%i
+set TCLCSVFNAME=tclcsv-%TCLCSVVER%
+
+@rem Windows binaries
+cd build && zip -r %TCLCSVFNAME%-src.zip tclcsv%TCLCSVVER%  && cd ..
+
+@rem --no-decode option because we do not want \r\n in Unix distro
+hg archive build\%TCLCSVFNAME%-src.tar.gz -X ".hg*" --no-decode
+hg archive build\%TCLCSVFNAME%-src.zip -X ".hg*"
+
+set TCLCSVFNAME=
